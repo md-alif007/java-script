0ba4eb02d32665fa67300a3f6f0f3ee28b374c9d
md-alif007 <mohammadalif043@gmail.com>
2025-02-06 11:54:17 +0600

    first commit

---
+++ js_comparison/comparison.js
@@ -0,0 +1,36 @@
+/*number
+var sunGlass = 80;
+
+string
+var sub = "EEE";
+
+Boolean
+var passed = true; 
+
+camelcase
+var lastExamNumber = 89;*/
+
+
+/* comparison
+1. bigger >
+2. less <
+3. equal ==
+4. greater than or equal >=
+5. less than or eual <=
+6. Not equal !=
+--------------------------------
+7. and &&
+8. or || */
+
+
+var peyaraPrice = 40 ;
+var orangePrice = 200 ;
+
+console.log(peyaraPrice <= orangePrice);
+
+
+/* === */
+
+console.log(10 === '10');
+console.log(10 == '10');
+
