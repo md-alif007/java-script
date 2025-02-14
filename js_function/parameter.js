616be99b695056fab68759988289678e8a0dd183
md-alif007 <mohammadalif043@gmail.com>
2025-02-14 16:50:05 +0600

    intro to function

---
+++ js_function/parameter.js
@@ -0,0 +1,14 @@
+function square(number){
+    console.log('value of the parameter is ', number);
+    const sqr = number * number;
+    console.log('square of the given number is : ', sqr);
+}
+square(4);
+
+console.log('------------------------------------------')
+
+function add(num1, num2) {
+    const sum = num1 + num2;
+    console.log('The summetion is : ', sum);
+}
+add(10, 10)
