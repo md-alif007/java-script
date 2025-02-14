c0505c63c3706d49fc006df85573ecf3144fd456
md-alif007 <mohammadalif043@gmail.com>
2025-02-14 11:05:09 +0600

    get the objects properties

---
+++ js_comparison/GetObjectProperties.js
@@ -0,0 +1,19 @@
+const person = {
+    name : 'alif rahman',
+    age : 25,
+    profession : 'developer',
+    salary : 25000,
+    married : true,
+    'fav place': ['Bandarban', 'saint martin', 'coxs bazar']
+}
+
+// console.log(person);
+// console.log(person.profession);
+// const income = person.salary
+// console.log(income);
+
+// console.log(person['age']);
+// const boyosh = person['age']
+// console.log(boyosh)
+
+console.log(person["fav place"]);
