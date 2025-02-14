55e0313eb8537bece299d5a47a24f633698fd3af
md-alif007 <mohammadalif043@gmail.com>
2025-02-14 17:04:35 +0600

    how to use return value

---
+++ js_function/returnValue.js
@@ -0,0 +1,15 @@
+function tenTimes(number) {
+    const result = number * 10;
+    return result;
+}
+
+function cutHalf(num) {
+    const half = num / 2;
+    return half;
+}
+
+const output = tenTimes(5);
+console.log(output);
+
+const halfOutput = cutHalf(10);
+console.log(halfOutput);
