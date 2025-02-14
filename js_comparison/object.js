a8f839066564a0303c9b45f76d02c81f121a14da
md-alif007 <mohammadalif043@gmail.com>
2025-02-14 10:48:06 +0600

    object declaration

---
+++ js_comparison/object.js
@@ -0,0 +1,26 @@
+const age = 21;
+const schl = 'rifle square';
+const isPassed = true;
+// let isDeveloper;
+
+const subj = ['bangla', 'English', 'phy'];
+// const bottle = ['white' , 45];
+
+//object
+
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+const sub = {
+    name : Biology,
+    teacher: 'Rasheda mam',
+    date:'30 feb',
+    exams:{
+        name : 'final exams',
+        marks : 100,
+    }
+}
