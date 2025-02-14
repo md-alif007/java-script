414b59e2832bc9e4b4174ce74249ba69855ba33b
md-alif007 <mohammadalif043@gmail.com>
2025-02-14 11:56:12 +0600

    nested object access properties

---
+++ js_comparison/nestedObject.js
@@ -0,0 +1,25 @@
+const college = {
+    name : 'gsc',
+    class : ['11' , '12'],
+    event : ['science fair', 'workshop'],
+    dress : {
+        color : 'blue',
+        result : {
+            gpa : 5,
+            merit : 'first'
+        }
+    }
+}
+
+// console.log(college.dress);
+// console.log(college.dress.color);
+// console.log(college.dress.result.gpa);
+
+// college.event[2] = 'sports';
+// console.log(college.event);
+
+// college.dress.result.merit = 'winner'
+// console.log(college.dress.result.merit);
+
+// delete college.dress.color;
+// console.log(college);
